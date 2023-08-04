cf2565f00058f47dc88717df67d5df39fd5adfd8
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-04 14:15:44 +0700

    [commit] - commit

--- crawl.js
+++ crawl.js
@@ -1,7 +1,27 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFormHTML(htmlBody, baseUrl) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkEl of linkElements) {
+        console.log({ linkEl: linkEl.href });
+        // urls.push(linkEl);
+    }
+    return urls;
+}
+
 function normalizeURL(urlString) {
-    return urlString;
+    const urlObj = new URL(urlString);
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+    console.log({ hostPath });
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1);
+    }
+    return hostPath;
 }
 
 module.exports = {
-    normalizeURL: normalizeURL
+    normalizeURL: normalizeURL,
+    getURLsFormHTML: getURLsFormHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,45 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFormHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL", () => {
-    const input = "";
+    const input = "https://blog.boot.dev/path";
     const actual = normalizeURL(input);
-    const expected = "";
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip protocol", () => {
+    const input = "https://blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+
+});
+
+test("normalizeURL capitials", () => {
+    const input = "https://BLOG.boot.dev/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test("normalizeURL strip http", () => {
+    const input = "http://blog.boot.dev/path";
+    const actual = normalizeURL(input);
+    const expected = "blog.boot.dev/path";
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFormHTML", () => {
+    const inputHTMLBody = `
+        <html>
+            <body>
+                <a href="https://blog.boot.dev">Boot.dev Blog</a>
+            </body>
+        </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev";
+    const actual = getURLsFormHTML(inputHTMLBody, inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
 });
